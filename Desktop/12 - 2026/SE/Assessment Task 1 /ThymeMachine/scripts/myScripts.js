7480321d47f022e48e62a923c9d801b526430e19
WilliamDrayton <William.Drayton@spcc.nsw.edu.au>
2025-12-08 11:41:05 +1100

    fixed css styling and implementation of javascript

--- Desktop/12 - 2026/SE/Assessment Task 1 /ThymeMachine/scripts/myScripts.js
+++ Desktop/12 - 2026/SE/Assessment Task 1 /ThymeMachine/scripts/myScripts.js
@@ -6,3 +6,28 @@
 */
 
 
+	const storedUsername = "WilliamDrayton"; 
+    const storedPassword = btoa("987513"); 
+
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault(); 
+
+        const usernameInput = document.getElementById('username').value;
+        const passwordInput = btoa(document.getElementById('password').value); 
+
+        if(usernameInput === storedUsername && passwordInput === storedPassword) {
+            document.getElementById('loginResult').textContent = "Login Successful!";
+            document.getElementById('loginResult').classList.remove('text-danger');
+            document.getElementById('loginResult').classList.add('text-success');
+
+           
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 1000);
+
+        } else {
+            document.getElementById('loginResult').textContent = "Login Failed. Try Again.";
+            document.getElementById('loginResult').classList.remove('text-success');
+            document.getElementById('loginResult').classList.add('text-danger');
+        }
+    });
