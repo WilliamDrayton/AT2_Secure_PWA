a81ea3649e24fd7a00925e055f45d866a66c0e83
WilliamDrayton <William.Drayton@spcc.nsw.edu.au>
2026-02-13 13:50:13 +1100

    added sql functionality

--- Desktop/12 - 2026/SE/PWA_with_python/app.py
+++ Desktop/12 - 2026/SE/PWA_with_python/app.py
@@ -1,6 +1,9 @@
 import re
-from flask import Flask, render_template
-from flask import jsonify
+from flask import Flask, render_template, jsonify
+import sqlite3
+
+
+
 
 app = Flask(__name__)
 
@@ -24,3 +27,60 @@ def products():
         {"id": 2, "name": "Blue Backpack", "price": 34.50},
         {"id": 3, "name": "Notebook", "price": 5.75}
     ])
+
+def get_db_connection():
+    # Creates a connection to the SQLite database
+    # row_factory allows us to access rows by name
+    connection = sqlite3.connect("app.db")
+    connection.row_factory = sqlite3.Row
+    return connection
+
+@app.route("/test-db")
+def test_db():
+    return jsonify({"status": "Database connection successful"})
+
+#@app.route("/setup-db")
+#def setup_db():
+    #connection = get_db_connection()
+    #cursor = connection.cursor()
+
+    #cursor.execute("""
+        #CREATE TABLE IF NOT EXISTS user (
+             #userID INTEGER PRIMARY KEY AUTOINCREMENT,
+              #fName TEXT NOT NULL,
+              #lName TEXT NOT NULL,
+              #email TEXT NOT NULL, --removed unique for testing
+              #userName TEXT NOT NULL,
+              #password TEXT NOT NULL
+#)
+   #""")
+
+    #connection.commit()
+    #connection.close()
+    #return "Database setup complete"
+
+
+
+@app.route("/add-sample")
+def add_sample():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        "INSERT INTO user (fName, lName, userName, email, password) VALUES (?, ?, ?, ?, ?)",
+       ("David", "Davison", "Davo", "s_Davison@yahoo.com", "weWillHashThisPassword")
+    )
+
+    connection.commit()
+    connection.close()
+    return "Sample data added"
+
+@app.route("/api/users")
+def get_users():
+    connection = get_db_connection()
+    items = connection.execute(
+        "SELECT userName, password FROM user"
+    ).fetchall()
+    connection.close()
+
+    return jsonify([dict(item) for item in items])
--- Desktop/12 - 2026/SE/PWA_with_python/static/myScript.js
+++ Desktop/12 - 2026/SE/PWA_with_python/static/myScript.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', init, false);
 function init() {
   console.log('page loaded')
   loadProducts();
+  loadUsers();
 }
 
 async function loadProducts() {
@@ -9,4 +10,19 @@ async function loadProducts() {
     const data = await res.json();
     document.getElementById("product-list").innerHTML =
         data.map(p => `<p>${p.name} - $${p.price}</p>`).join("");
+}
+
+async function loadUsers(){
+  try{
+    const response = await fetch('/api/users');
+    if(!response.ok){
+      throw new Error('HTTP error! status: ${response.status}');
+    }
+    const data = await response.json();
+    console.log("Fetched users");
+    document.getElementById('user-list').innerHTML =
+    data.map(p => `<p>${p.userName} - ${p.password}</p>`).join("");
+  }catch (error) {
+        console.error("Error fetching users:", error);
+    }
 }
